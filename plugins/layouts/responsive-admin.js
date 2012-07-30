9e991bead660d0fc59f0546aa4a83d36835d3c67
Gábor Hojtsy <gabor@hojtsy.hu>
2012-07-30 21:08:17 +0200

    Serialize settings into JSON; implement column data passing from server to client (not yet to the way back)
    Still has issues not getting the proper list of regions when regions are removed and not getting proper column info  yet.

--- plugins/layouts/responsive-admin.js
+++ plugins/layouts/responsive-admin.js
@@ -12,21 +12,6 @@ function log (message, type) {
   }
 }
 
-/**
- * Respond to app updates.  This is generic so we don't have to
- * a callback for each event we'd like to track during prototyping.
- */
-function ResponsiveLayoutDesignerEventHandler (event) {
-  var i;
-  // Log the event type.
-  log(event.type, 'info');
-  var args = Array.prototype.slice.call(arguments);
-  // Print the args as well.
-  for (i = 0; i < args.length; i++) {
-    log(args[i], 'dir');
-  }
-}
-
 Drupal.responsiveLayout = Drupal.responsiveLayout || {};
 
 Drupal.behaviors.responsiveLayoutAdmin = {
@@ -40,11 +25,52 @@ Drupal.behaviors.responsiveLayoutAdmin = {
  * Initialize responsive layout editor.
  */
 Drupal.responsiveLayout.init = function() {
+  // Initialize region list and per-breakpoint columns.
+  var regionList = [];
+  var layoutConfig = $.parseJSON($('#edit-layout-settings-layout-responsive-regions').val());
+  for (var regionIndex in layoutConfig.regions) {
+    regionList.push({
+      'machine_name': layoutConfig.regions[regionIndex].name,
+      'label': layoutConfig.regions[regionIndex].admin_title,
+    });
+  }
+
+  // Build a list of grids for the editor.
+  var gridList = [];
+  for (var gridIndex in Drupal.settings.responsiveLayout.defaultGrids) {
+    gridList.push({
+      'machine_name': Drupal.settings.responsiveLayout.defaultGrids[gridIndex].name,
+      'columns': Drupal.settings.responsiveLayout.defaultGrids[gridIndex].columns,
+      'classes': ['rld-container-' + Drupal.settings.responsiveLayout.defaultGrids[gridIndex].name],
+    });
+  }
+
+  // Build a list of breakpoints for the editor.
+  var breakpointList = [];
+  for (var breakpointIndex in Drupal.settings.responsiveLayout.defaultBreakpoints) {
+    var overrideList = [];
+    var name = Drupal.settings.responsiveLayout.defaultBreakpoints[breakpointIndex].name;
+    for (var overrideIndex in layoutConfig.overrides[name]) {
+      overrideList.push({
+        'machine_name': layoutConfig.overrides[name][overrideIndex].name,
+        'columns': layoutConfig.overrides[name][overrideIndex].columns,
+      });
+    }
+    breakpointList.push({
+      'label': Drupal.settings.responsiveLayout.defaultBreakpoints[breakpointIndex].admin_title,
+      'machine_name': name,
+      // @todo: make sure that em/px based breakpoints work alike.
+      'breakpoint': parseInt(Drupal.settings.responsiveLayout.defaultBreakpoints[breakpointIndex].width),
+      'grid': Drupal.settings.responsiveLayout.defaultBreakpoints[breakpointIndex].grid_name,
+      'regions': overrideList,
+    });
+  }
+
   // Instantiate a layout designer.
   this.editor = new ResponsiveLayoutDesigner({
-    'regions': Drupal.responsiveLayout.getRegionList(),
-    'steps': Drupal.responsiveLayout.getLayoutConfig(),
-    'grids': Drupal.responsiveLayout.getGridList(),
+    'regions': regionList,
+    'steps': breakpointList,
+    'grids': gridList,
   });
 
   // var save = $.proxy(this.save, this);
@@ -52,13 +78,13 @@ Drupal.responsiveLayout.init = function() {
   // Register event listeners. Just update our representation of the layout
   // for any event for now.
   this.editor.registerEventListener({
-    'regionOrderUpdated': ResponsiveLayoutDesignerEventHandler,
-    'layoutSaved': ResponsiveLayoutDesignerEventHandler,
-    'regionRemoved': ResponsiveLayoutDesignerEventHandler,
-    'regionAdded': ResponsiveLayoutDesignerEventHandler,
-    'regionResized': ResponsiveLayoutDesignerEventHandler,
-    'regionResizing': ResponsiveLayoutDesignerEventHandler,
-    'regionResizeStarted': ResponsiveLayoutDesignerEventHandler
+    'regionOrderUpdated': Drupal.responsiveLayout.eventHandler,
+    'layoutSaved': Drupal.responsiveLayout.eventHandler,
+    'regionRemoved': Drupal.responsiveLayout.eventHandler,
+    'regionAdded': Drupal.responsiveLayout.eventHandler,
+    'regionResized': Drupal.responsiveLayout.eventHandler,
+    'regionResizing': Drupal.responsiveLayout.eventHandler,
+    'regionResizeStarted': Drupal.responsiveLayout.eventHandler
   });
 
   // Insert the editor in the DOM.
@@ -74,99 +100,74 @@ Drupal.responsiveLayout.init = function() {
 }
 
 /**
- * Responds to the ResponsiveLayoutDesigner's 'layoutSaved' event.
+ * Respond to app updates.
+ *
+ * This is generic so we don't have to have a callback for each event we'd like
+ * to track during prototyping.
  */
-Drupal.responsiveLayout.save = function (event) {
-  var layoutManager = event.data.responsiveLayout.editor.save();
+Drupal.responsiveLayout.eventHandler = function(event) {
+  /*var i;
+  // Log the event type.
+  log(event.type, 'info');
+  var args = Array.prototype.slice.call(arguments);
+  // Print the args as well.
+  for (i = 0; i < args.length; i++) {
+    log(args[i], 'dir');
+  }*/
+
+  var layoutSettings = {'regions' : [], 'overrides': []};
+  var layoutManager = Drupal.responsiveLayout.editor.save();
   var regionList = layoutManager.info('regionList');
   var regions = regionList.info('items');
-  var regionText = event.data.responsiveLayout.setRegionList(regions);
-  // Set the new region textfield value on the form elements.
-  var $hiddenText = $(this).find('#edit-layout-settings-layout-responsive-regions');
-  var value = $hiddenText.val();
-  $hiddenText.val(regionText);
-  value = $hiddenText.val();
-  return true;
-};
+  for (var i = 0; i < regions.length; i++) {
+    layoutSettings.regions.push({'name': regions[i].info('machine_name'), 'admin_title': regions[i].info('label')});
+  }
 
-/**
- * Returned parsed list of regions based on values in textarea.
- */
-Drupal.responsiveLayout.getRegionList = function() {
-  var regionList = [];
-  var editorLines = $('#edit-layout-settings-layout-responsive-regions').val().split("\n");
-  var lineNo, line;
-  for (lineNo in editorLines) {
-    line = editorLines[lineNo];
-    if (line.length) {
-      // Regions are represented as 'name;classes'.
-      var regionDefinition = line.split('; ', 3);
-      regionList.push({
-        'machine_name': regionDefinition[0],
-        'label': regionDefinition[1],
-        'classes': regionDefinition[2]
-      });
+  var stepList = layoutManager.info('stepList');
+  var steps = stepList.info('items');
+  for (var i = 0; i < steps.length; i++) {
+    layoutSettings.overrides[steps[i].machine_name] = {};
+    if (steps[i].regionList.items.length) {
+      for (var r = 0; r < steps[i].regionList.items.length; r++) {
+        layoutSettings.overrides[steps[i].machine_name].push({
+          'name': steps[i].regionList.items[r].machine_name,
+          'columns': steps[i].regionList.items[r].columns
+        });
+      }
     }
   }
-  return regionList;
-}
 
-/**
- * Returned processed list of grids to present to the layout editor.
- */
-Drupal.responsiveLayout.getGridList = function() {
-  var gridList = [];
-  for (g in Drupal.settings.responsiveLayout.defaultGrids) {
-    gridList.push({
-      'machine_name': Drupal.settings.responsiveLayout.defaultGrids[g].name,
-      'columns': Drupal.settings.responsiveLayout.defaultGrids[g].columns,
-      'classes': ['rld-container-' + Drupal.settings.responsiveLayout.defaultGrids[g].name],
-    });
-  }
-  return gridList;
-}
-
-/**
- * Returned processed list of breakpoints/steps to present to the layout editor.
- */
-Drupal.responsiveLayout.getLayoutConfig = function() {
-  var breakpointList = [];
-  for (b in Drupal.settings.responsiveLayout.defaultBreakpoints) {
-    breakpointList.push({
-      'label': Drupal.settings.responsiveLayout.defaultBreakpoints[b].admin_title,
-      'machine_name': Drupal.settings.responsiveLayout.defaultBreakpoints[b].name,
-      'breakpoint': parseInt(Drupal.settings.responsiveLayout.defaultBreakpoints[b].width),
-      'grid': Drupal.settings.responsiveLayout.defaultBreakpoints[b].grid_name,
-      'regions': [
-        {
-          'machine_name': 'header_a',
-          'columns': 1
-        },
-        {
-          'machine_name': 'header_b',
-          'columns': 2
-        },
-      ],
-    });
-  }
-  return breakpointList;
-}
-
-/**
- * Save region list to local textarea that is used for server communication.
- *
- * The value of the textarea is saved to the server when the whole layout is
- * saved. We do not have live AJAX communication because the interaction is
- * built with rapid changes in mind (ordering, adding new regions, resizing),
- * and we don't have a live preview needed given the useful builder view
- * itself.
- */
-Drupal.responsiveLayout.setRegionList = function(regions) {
-  var regionsText = '';
-  for (var i = 0; i < regions.length; i++) {
-    regionsText += regions[i].info('machine_name') + '; ' + regions[i].info('label') + '; ' + regions[i].info('classes') + "\n";
-  }
-  return regionsText;
+  // The value of the textarea is saved to the server when the whole layout is
+  // saved. We do not have live AJAX communication because the interaction is
+  // built with rapid changes in mind (ordering, adding new regions, resizing),
+  // and we don't have a live preview needed given the useful builder view
+  // itself.
+  $('#edit-layout-settings-layout-responsive-regions').val($.serializeJSON(layoutSettings));
 }
 
 })(jQuery, ResponsiveLayoutDesigner);
+
+jQuery(function($) {
+    $.extend({
+        serializeJSON: function(obj) {
+            var t = typeof(obj);
+            if(t != "object" || obj === null) {
+                // simple data type
+                if(t == "string") obj = '"' + obj + '"';
+                return String(obj);
+            } else {
+                // array or object
+                var json = [], arr = (obj && obj.constructor == Array);
+
+                $.each(obj, function(k, v) {
+                    t = typeof(v);
+                    if(t == "string") v = '"' + v + '"';
+                    else if (t == "object" & v !== null) v = $.serializeJSON(v)
+                    json.push((arr ? "" : '"' + k + '":') + String(v));
+                });
+
+                return (arr ? "[" : "{") + String(json) + (arr ? "]" : "}");
+            }
+        }
+    });
+});
